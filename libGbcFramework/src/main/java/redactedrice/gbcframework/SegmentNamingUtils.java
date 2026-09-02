edc5ee9d62a3040b00e39273747f3a437b40c935
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-09-02 15:55:50 -0400

    Cleaning and remarking todos

--- libGbcFramework/src/main/java/redactedrice/gbcframework/SegmentNamingUtils.java
+++ libGbcFramework/src/main/java/redactedrice/gbcframework/SegmentNamingUtils.java
@@ -27,7 +27,6 @@ public final class SegmentNamingUtils {
     }
 
     public static String replacePlaceholders(String line, Map<String, String> placeholderToArgs) {
-        // TODO: refactor to find placeholder in line and search map for that
         for (Entry<String, String> entry : placeholderToArgs.entrySet()) {
             if (!entry.getKey().startsWith(PLACEHOLDER_MARKER)
                     || !entry.getKey().endsWith(PLACEHOLDER_MARKER)) {
